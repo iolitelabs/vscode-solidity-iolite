8dc872478b06a4c9407da800335a0ffc2b334567
Vladyslav Makarian <vladislav@iolite.io>
2018-04-12 02:38:59 +0300

    iteration fixes, fixes after merge

--- src/projectService.ts
+++ src/projectService.ts
@@ -21,7 +21,7 @@ function createPackage(rootPath: string) {
         // TODO: automapper
         let packageConfig = readyaml.sync(projectPackageFile);
         // TODO: throw expection / warn user of invalid package file
-        let projectPackage = new Package(packageDependenciesContractsDirectory);
+        let projectPackage = new Package();
         projectPackage.absoluletPath = rootPath;
         if (packageConfig) {
             if (packageConfig.layout !== undefined) {
@@ -104,7 +104,7 @@ function getDirectories(dirPath: string): string[] {
 }
 
 function createDefaultPackage(packagePath: string): Package {
-    let defaultPackage = new Package(packageDependenciesContractsDirectory);
+    let defaultPackage = new Package();
     defaultPackage.absoluletPath = packagePath;
     defaultPackage.name = path.basename(packagePath);
     return defaultPackage;
